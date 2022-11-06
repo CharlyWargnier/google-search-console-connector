e11f45f94f49e045ebe2f7f3c29f28c61c110e67
For Glory <forglory09051@gmail.com>
2022-11-06 00:56:48 -0700

    add _RELEASE configuration

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,16 +38,26 @@ RowCap = 25000
 
 ###############################################################################
 
-_component_func = components.declare_component(
-    # We give the component a simple, descriptive name ("my_component"
-    # does not fit this bill, so please choose something better for your
-    # own component :)
-    "my_component",
-    # Pass `url` here to tell Streamlit that the component will be served
-    # by the local dev server that you run via `npm run start`.
-    # (This is useful while your component is in development.)
-    url="http://localhost:3001",
-)
+_RELEASE = False
+
+if not _RELEASE:
+    _component_func = components.declare_component(
+        # We give the component a simple, descriptive name ("my_component"
+        # does not fit this bill, so please choose something better for your
+        # own component :)
+        "my_component",
+        # Pass `url` here to tell Streamlit that the component will be served
+        # by the local dev server that you run via `npm run start`.
+        # (This is useful while your component is in development.)
+        url="http://localhost:3001",
+    )
+else:
+    # When we're distributing a production version of the component, we'll
+    # replace the `url` param with `path`, and point it to to the component's
+    # build directory:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _component_func = components.declare_component("my_component", path=build_dir)
 
 def my_component(name, key=None):
     component_value = _component_func(name=name, key=key, default='')
